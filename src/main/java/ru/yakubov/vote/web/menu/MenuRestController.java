0dff5f6cbdb68aaf97754dd8e34cc7add5835a13
source-store <73580046+source-store@users.noreply.github.com>
2021-03-29 12:58:19 +0300

    fix

--- src/main/java/ru/yakubov/vote/web/menu/MenuRestController.java
+++ src/main/java/ru/yakubov/vote/web/menu/MenuRestController.java
@@ -2,7 +2,11 @@ package ru.yakubov.vote.web.menu;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 import ru.yakubov.vote.model.Menu;
 import ru.yakubov.vote.service.MenuService;
 
